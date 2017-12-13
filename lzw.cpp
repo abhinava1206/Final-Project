78de9e2e58972b8c7e95ee525cbb37eab7d1ee4b
abhinava1206 <abhinava0001@gmail.com>
2017-12-12 19:37:21 -0800

    Update lzw.cpp
    Now includes comments

--- lzw.cpp
+++ lzw.cpp
@@ -8,18 +8,18 @@ void compress( ifstream& in, ofstream& out )
 {
   std::unordered_map<std::string,unsigned int> codes;
   for ( unsigned int i = 0 ; i < 256 ; i++ )
-    codes[std::string(1,(char)i)] = i;
+    codes[std::string(1,(char)i)] = i; // makes the standard dictionary with ASCII values
 
   unsigned int next_index = 257;
   std::string current_string;
   char c;
-  while (!in.eof()) {
+  while (!in.eof()) { // reads from input file 
     
     in.read(&c, sizeof(char));
   
-    current_string = current_string + c;
-    if ( codes.find(current_string) == codes.end() ) {
-      codes[ current_string ] = next_index++;
+    current_string = current_string + c; // appends the character "c" to the current pattern
+    if ( codes.find(current_string) == codes.end() ) { // if the pattern has not been found in the dictionary
+      codes[ current_string ] = next_index++; 
       current_string.erase(current_string.size()-1);
       out.write((const char*) (&(codes[current_string])), sizeof(unsigned int));
       current_string = c;
@@ -32,7 +32,7 @@ void decompress( ifstream& in, ofstream& out )
 {
   std::unordered_map<unsigned int,std::string> strings;
   for ( int unsigned i = 0 ; i < 256 ; i++ )
-    strings[i] = std::string(1,i);
+    strings[i] = std::string(1,i); // again we construct a standard ASCII dictionary
 
   std::string previous_string;
   std::string current_string;
@@ -43,8 +43,8 @@ void decompress( ifstream& in, ofstream& out )
 
   while (!in.eof()) {
     
-    in.read((char*)&code, sizeof(unsigned int));
-    if ( strings.find( code ) == strings.end() ) 
+    in.read((char*)&code, sizeof(unsigned int)); 
+    if ( strings.find( code ) == strings.end() )  // has the opposite effect as compression
       strings[ code ] = previous_string + previous_string[0];
     out << strings[code];
     if ( previous_string.size() )
@@ -56,7 +56,7 @@ void decompress( ifstream& in, ofstream& out )
 
 int main()
 {
-  ifstream fp_in;  
+  ifstream fp_in;   // sample main I made for debugging
   ofstream fp_out;
   fp_in.open("test.txt", ios::in);    
   fp_out.open("file3.txt", ios::out);
